d129fb7c1015c583c56e0e1517a57ea2a5a2137e
Gem Louie Angelo Denola <gemlouieangelodenola@example.com>
2023-09-24 01:33:45 +0800

    2nd Change
    Sept 23, 2023

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = '';
-$config['base_url'] = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') .$_SERVER['HTTP_HOST']."/GemLouieAngeloDenola";
+$config['base_url'] = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') .$_SERVER['HTTP_HOST']."/GemLouieAngeloDenola1";
 /*
 |--------------------------------------------------------------------------
 | Index File
